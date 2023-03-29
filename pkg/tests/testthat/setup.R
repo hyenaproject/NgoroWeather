abd81e3472c974f80ea7b801820f677c1a97fd14
Liam Bailey <liam.bailey@liamdbailey.com>
2023-03-29 14:47:39 +0200

    Tests now load data

--- pkg/tests/testthat/setup.R
+++ pkg/tests/testthat/setup.R
@@ -1,2 +1,5 @@
 ## If we need to remove any objects from global
-# withr::defer(remove(weather_data), teardown_env())
+withr::defer(remove(.database), teardown_env())
+
+load_package_database.weather(system.file("extdata/working_weather", package = "NgoroWeather"),
+                              overwrite.db = "yes")
